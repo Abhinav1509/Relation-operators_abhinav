9e6d3c3160b8ed23e88819515d823bd9027138ec
Abhinav1509 <104301528+Abhinav1509@users.noreply.github.com>
2022-05-08 22:43:48 +0530

    Update Relation operators.c

--- Relation operators.c
+++ Relation operators.c
@@ -1,25 +1,20 @@
 #include <stdio.h>
 int main()
 {
-    int a = 5, b = 5, c = 10, result;
+    int a = 5, b = 5, c = 10;
 
-    result = (a == b) && (c > b);
-    printf("(a == b) && (c > b) is %d \n", result);
-
-    result = (a == b) && (c < b);
-    printf("(a == b) && (c < b) is %d \n", result);
-
-    result = (a == b) || (c < b);
-    printf("(a == b) || (c < b) is %d \n", result);
-
-    result = (a != b) || (c < b);
-    printf("(a != b) || (c < b) is %d \n", result);
-
-    result = !(a != b);
-    printf("!(a != b) is %d \n", result);
-
-    result = !(a == b);
-    printf("!(a == b) is %d \n", result);
+    printf("%d == %d is %d \n", a, b, a == b);
+    printf("%d == %d is %d \n", a, c, a == c);
+    printf("%d > %d is %d \n", a, b, a > b);
+    printf("%d > %d is %d \n", a, c, a > c);
+    printf("%d < %d is %d \n", a, b, a < b);
+    printf("%d < %d is %d \n", a, c, a < c);
+    printf("%d != %d is %d \n", a, b, a != b);
+    printf("%d != %d is %d \n", a, c, a != c);
+    printf("%d >= %d is %d \n", a, b, a >= b);
+    printf("%d >= %d is %d \n", a, c, a >= c);
+    printf("%d <= %d is %d \n", a, b, a <= b);
+    printf("%d <= %d is %d \n", a, c, a <= c);
 
     return 0;
 }
